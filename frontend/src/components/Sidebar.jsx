cc8d874b09c1751b3ef08bb6294caec159a99cd0
Susmit Mukherjee <75859651+SusmitM@users.noreply.github.com>
2023-10-05 02:56:58 +0530

    Github navigation added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,7 +3,8 @@ import {RxDashboard} from 'react-icons/rx';
 import {CgGym} from 'react-icons/cg';
 import {PiBowlFoodFill} from 'react-icons/pi';
 import {GoGoal} from 'react-icons/go';
-import { useNavigate } from 'react-router-dom';
+import {BsGithub} from 'react-icons/bs';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
     const navigate=useNavigate();
@@ -14,6 +15,8 @@ const Sidebar = () => {
       <li onClick={()=>navigate("/exercise")} className="flex flex-wrap justify-center gap-2 cursor-pointer"><span ><CgGym size={20}/></span>Exercise Tracking</li>
       <li onClick={()=>navigate("/food")} className="flex flex-wrap justify-center gap-2 cursor-pointer"><span><PiBowlFoodFill size={20}/></span>Food Tracking</li>
       <li onClick={()=>navigate("/goal")} className="flex flex-wrap justify-center gap-2 cursor-pointer"><span><GoGoal size={20}/></span>Goal Tracking</li>
+      <li onClick={()=>navigate("/goal")} className="flex flex-wrap justify-center gap-2 cursor-pointer"><span><BsGithub size={20}/></span> <Link to="https://github.com/SusmitM/Fitbit" target="_blank">Github</Link></li>
+     
     </ul>
   </div>
   )
