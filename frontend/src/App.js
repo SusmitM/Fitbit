e8645f8e790732ec641c8e9b8ae4217f30b11c07
Susmit Mukherjee <75859651+SusmitM@users.noreply.github.com>
2023-10-04 20:39:50 +0530

    feat: tables added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import AllRoutes from "./routes/AllRoutes";
 
 function App() {
+  
   return (
     <div className="App">
       <AllRoutes />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { DataContextProvider } from "./context/DataContext";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Provider store={store}>
+        <DataContextProvider>
+          <App />
+        </DataContextProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
